2da72d822d850a2f7f525e2c320e0bd286376bf8
Vedant <pvedant110@gmail.com>
2024-11-01 11:07:40 +0530

    script

--- app.js
+++ app.js
@@ -9,6 +9,37 @@ app.get("/hi", (req, res) => res.send("hello world"))
 
 app.get("/hello", (req, res) => res.send(jsonData))
 
+app.get("/scrape-data", (req, res) => {
+  try {
+    console.log("process initiated")
+    // Execute the Python script
+    exec("python script.py", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing Python script: ${error.message}`)
+        // return res.status(500).json({ error: "Failed to execute script" })
+      }
+
+      // Path to the generated JSON file
+      const filePath = path.join(__dirname, "./jsonData/scraped_data.json")
+
+      // Check if file exists
+      fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+          console.error(`Error reading JSON file: ${err.message}`)
+          // return res.status(500).json({ error: "Failed to read JSON data" })
+        }
+
+        // Send JSON data to the frontend
+        res.setHeader("Content-Type", "application/json")
+        res.send(data)
+      })
+    })
+  } catch (error) {
+    console.log(error)
+    res.errored(error)
+  }
+})
+
 const server = app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
 )
