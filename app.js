240a05a3d1a7d3104cb61516828f65654053784a
Vedant <pvedant110@gmail.com>
2024-11-01 12:10:26 +0530

    api added

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 const express = require("express")
 const app = express()
-const port = process.env.PORT || 3001
+const { exec } = require("child_process")
+const fs = require("fs")
+const path = require("path")
+const port = 3000
+const fallBackJson = require("./jsonData/scraped_data.json")
 
-app.get("/", (req, res) => res.type("html").send(html))
+app.get("/scrape-data", (req, res) => {
+  try {
+    console.log("process initiated")
 
-const server = app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-)
+    // Execute the Python script
+    exec("python script.py", (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing Python script: ${error.message}`)
+        return res.status(500).json({ error: "Failed to execute script" })
+      }
 
-server.keepAliveTimeout = 120 * 1000
-server.headersTimeout = 120 * 1000
+      // Path to the generated JSON file
+      const filePath = path.join(__dirname, "./jsonData/scraped_data.json")
 
-const html = "Hi"
+      // Check if file exists
+      fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+          console.error(`Error reading JSON file: ${err.message}`)
+          return res.status(500).json({ error: "Failed to read JSON data" })
+        }
+
+        // Send JSON data to the frontend
+        res.setHeader("Content-Type", "application/json")
+        res.send(data)
+      })
+    })
+  } catch (error) {
+    console.log(error)
+    res.errored(error)
+  }
+})
+
+app.get("/fetch-data", (req, res) => {
+  console.log("Fetching data...")
+
+  try {
+    res.status(200).json(fallBackJson)
+  } catch (error) {
+    console.error("Error fetching data:", error)
+    res.status(500).json(fallBackJson)
+  }
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
