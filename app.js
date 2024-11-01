e14252aaa3fbe530bdc5afb82daab7deb12b4aed
Vedant <pvedant110@gmail.com>
2024-11-01 10:55:47 +0530

    json

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3001
+const jsonData = require("./jsonData/scraped_data.json")
 
 app.get("/", (req, res) => res.type("html").send(html))
 
 app.get("/hi", (req, res) => res.send("hello world"))
+
+app.get("/hello", (req, res) => res.send(jsonData))
 
 const server = app.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
